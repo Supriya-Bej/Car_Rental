5db3ab188104f9f81771df0d46086500acd3f9be
Supriya-Bej <supriyabej95@gmail.com>
2026-01-02 13:19:20 +0530

    initial push

---
+++ script.js
@@ -0,0 +1,11 @@
+gsap.to('.service .service-place carousel-item',{
+    x:1300,
+    duration:2,
+    delay:1,
+    backgroundColor:'red',
+    rotate:360,
+    borderRadious:'50%',
+    scale:0.5
+})
+
+var swiper = new Swiper('.swiper')
